82b16320098537b7580b6dfe419f9e450d5d3d5d
optimist93 <44784805+optimist93@users.noreply.github.com>
2019-07-23 11:05:20 +0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# evexcon
+# casablanca
---
+++ src/js/some.js
@@ -0,0 +1,90 @@
+
+$(function() {
+
+	$('.main-slider').slick({
+		arrows: false,
+		dots: true,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+	});
+
+	$('.communities').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		nextArrow: '<div class="arrow arrow_right"><img src="build/img/shevron-right.svg" alt="стрелка"></div>',
+		prevArrow: '<div class="arrow arrow_left"><img src="build/img/shevron-left.svg" alt="стрелка"></div>',
+		responsive: [
+			{
+				breakpoint: 1199,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 630,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+	$('.work-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		nextArrow: '<div class="arrow arrow_right"><img src="build/img/our-work/work_right.svg" alt="стрелка"></div>',
+		prevArrow: '<div class="arrow arrow_left"><img src="build/img/our-work/work_left.svg" alt="стрелка"></div>',
+	});
+	$('.farm-list').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		nextArrow: '<div class="arrow arrow_right"><img src="build/img/shevron-right.svg" alt="стрелка"></div>',
+		prevArrow: '<div class="arrow arrow_left"><img src="build/img/shevron-left.svg" alt="стрелка"></div>',
+	});
+	$('.contacts-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		nextArrow: '<div class="arrow arrow_right"><img src="build/img/our-work/work_right.svg" alt="стрелка"></div>',
+		prevArrow: '<div class="arrow arrow_left"><img src="build/img/our-work/work_left.svg" alt="стрелка"></div>'
+	});
+	$('.schools-slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: '<div class="arrow arrow_right">&rarr;</div>',
+		prevArrow: '<div class="arrow arrow_left">&larr;</div>',
+		responsive: [
+			{
+				breakpoint: 1600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
+}); // end jquery
