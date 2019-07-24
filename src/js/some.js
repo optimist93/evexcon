7357f675eeeec771bf0ebc6c0510e1e224334a62
optimist93 <44784805+optimist93@users.noreply.github.com>
2019-07-24 06:46:22 +0500

    add modal

--- src/js/some.js
+++ src/js/some.js
@@ -86,5 +86,50 @@ $(function() {
 			}
 		]
 	});
+	$('.modal-portfolio_list').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: '<div class="arrow arrow_right"><img src="build/img/our-work/work_right.svg" alt="стрелка"></div>',
+		prevArrow: '<div class="arrow arrow_left"><img src="build/img/our-work/work_left.svg" alt="стрелка"></div>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+	$('.modal-communities').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		nextArrow: '<div class="arrow arrow_right"><img src="build/img/shevron-right.svg" alt="стрелка"></div>',
+		prevArrow: '<div class="arrow arrow_left"><img src="build/img/shevron-left.svg" alt="стрелка"></div>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+	$('.portfolio-list li').on('click', function() {
+		$('.modal-overlay').addClass('modal-overlay-show');
+	});
+	$('.modal-close').on('click', function() {
+		$('.modal-overlay').removeClass('modal-overlay-show');
+	});
 
 }); // end jquery
